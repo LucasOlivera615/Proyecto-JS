458fadde84e01ae81c8842c96a059f202415b3d7
LucasOlivera615 <lo.lucasolivera@gmail.com>
2025-07-22 10:35:14 -0300

    añadido de funciones y refinamiento del codigo, modificacion en el style y el index

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,15 @@
 const agendados = []
 
+function pedirFecha (mensaje, mensaje2, condicion1, condicion2, condicion3) { //Funcion para simplificar la funcion datos y no repetir codigo
+    let fecha = prompt(mensaje)
+    while (fecha.length !== condicion1 || parseInt(fecha) != fecha || parseInt(fecha) < condicion2 || parseInt(fecha) > condicion3) {
+        alert (mensaje2)
+        fecha = prompt(mensaje)
+    }
+
+    return fecha
+}
+
 function datos () { //Función que crea una persona a partir de los datos que el usuario envía
     let nombre = prompt("Escribe tu nombre completo. (nombre y apellido)").toLowerCase()
     let ci = prompt("Escribe tu cedula de identidad. (sin puntos ni guiones.)")
@@ -7,27 +17,17 @@ function datos () { //Función que crea una persona a partir de los datos que el
         alert ("Cédula de identidad inválida, recuerda que la cédula debe tener máximo 8 digitos")
         ci = prompt("Escribe tu cedula de identidad. (sin puntos ni guiones.)")
     }
-    let dia = prompt("Escribe el día del calendario en el que quieras venir (en número de 2 dígitos)")
-    while (dia.length !== 2) {
-        alert ("El día es inválido, debe tener 2 digitos")
-        dia = prompt("Escribe el día del calendario en el que quieras venir (en número de 2 dígitos)")
-    }
-    parseInt(dia)
-    let mes = prompt("Escribe el mes del calendario en el que quieras venir (en número de 2 dígitos)")
-    while (mes.length !== 2) {
-        alert ("El mes es inválido, debe tener 2 digitos")
-        mes = prompt("Escribe el mes del calendario en el que quieras venir (en número de 2 dígitos)")
-    }
-    parseInt(mes)
-    let año = prompt("Escribe el año del calendario en el que quieras venir (en número de 4 dígitos)")
-    while (año.length !== 4) {
-        alert ("El año es inválido, debe tener 4 digitos")
-        año = prompt("Escribe el año del calendario en el que quieras venir (en número de 4 dígitos)")
-    }
-    parseInt(año)
+    let dia = pedirFecha("Escribe el día del calendario en el que quieras venir (en número de 2 dígitos)", "El día es inválido, debe tener 2 digitos y estar entre 01 y 31", 2, 1, 31)
+    let mes = pedirFecha("Escribe el mes del calendario en el que quieras venir (en número de 2 dígitos)", "El mes es inválido, debe tener 2 digitos y estar entre 01 y 12", 2, 1, 12)
+    let año = pedirFecha("Escribe el año del calendario en el que quieras venir (en número de 4 dígitos)", "El año es inválido, debe tener 4 digitos y ser mayor o igual a 2025", 4, 2025)
     let fecha = dia + "/" + mes + "/" + año
+    let hora = prompt("Escribe la hora en la que quieres agendarte, debe ser entre 1pm y 5pm")
+    while (hora.length != 1 || hora < 1 || hora > 5) {
+        alert ("El horario que intentas ingresar es inválido.")
+        hora = prompt("Escribe la hora en la que quieres agendarte, debe ser entre 1pm y 5pm")
+    }
 
-    const persona = {nombre, ci, fecha}
+    const persona = {nombre, ci, fecha, hora}
 
     return persona
 }
@@ -37,7 +37,7 @@ function buscarAgendado (ci) { //Funcion que busca si un usuario en particular e
     for (const agendado of agendados) {
         if (agendado.ci == ci) {
             busqueda = true
-            alert("El usuario con la cédula de identidad " + ci + " está agendado para el " + agendado.fecha)
+            alert("El usuario con la cédula de identidad " + ci + " está agendado para el " + agendado.fecha + " a las " + agendado.hora + "pm.")
         }
     }
     if (!busqueda) {
@@ -46,13 +46,19 @@ function buscarAgendado (ci) { //Funcion que busca si un usuario en particular e
 }
 
 function fechasOcupadas () { //Funcion que muestra mediante alertas, las fechas que ya han sido ocupadas.
-    alert ("Las fechas ya ocupadas son las siguientes:\n")
-    for (const agendado of agendados) {
-        alert(agendado.fecha)
+    if (agendados.length == 0) {
+        alert("Aún no hay fechas ni horarios ocupados.")
+        return
     }
+
+    let mensaje = "Las fechas y horarios ya ocupados son los siguientes:\n\n"
+    for (const agendado of agendados) {
+        mensaje = mensaje + agendado.fecha + " - " + agendado.hora + "pm.\n"
+    }
+    alert(mensaje)
 }
 
-let pregunta = prompt("Pulsa el número correspondiente para navegar entre las distintas opciones: \n1 - Agendar a un usuario. \n2 - Consultar en qué fecha está agendado un usuario. \n3 - Consultar qué fechas ya están ocupadas. \n4 - Salir.").toLowerCase()
+let pregunta = prompt("¡Bienvenido al menú del consultorio odontológico Jardines!\n\n Pulsa el número correspondiente para navegar entre las distintas opciones: \n1 - Agendar a un usuario. \n2 - Consultar en qué fecha está agendado un usuario. \n3 - Consultar qué fechas ya están ocupadas. \n4 - Salir.")
 
 while (pregunta !== "4") {
     switch (pregunta) {
@@ -60,17 +66,20 @@ while (pregunta !== "4") {
             let nuevaPersona = datos()
             let busqueda = false
             for (const agendado of agendados) {
-                if (agendado.ci == nuevaPersona.ci) {
+                if (agendado.ci === nuevaPersona.ci) {
                     busqueda = true
-                    alert ("La persona que intentas agendar ya tiene una fecha, su fuecha es: "+ agendado.fecha)
-                } else if (agendado.fecha === nuevaPersona.fecha) {
-                    busqueda = true
-                    alert ("Esa fecha ya está ocupada, elige otra fecha.")
+                    alert ("La persona que intentas agendar ya tiene una fecha, su fecha es: "+ agendado.fecha)
+                }
+                if (agendado.fecha === nuevaPersona.fecha) {
+                    if (agendado.hora === nuevaPersona.hora) {
+                        busqueda = true
+                        alert("Esta hora ya está ocupada, selecciona otro horario u otra fecha.")
+                    }
                 }
             }
             if (!busqueda) {
                 agendados.push (nuevaPersona)
-                alert ("Felicidades, la fecha es tuya, puedes venir el: " + nuevaPersona.fecha)
+                alert ("Felicidades, la fecha es tuya, puedes venir el: " + nuevaPersona.fecha + " en el horario de " + nuevaPersona.hora + "pm a " + nuevaPersona.hora + ":30pm.")
             }
 
             break
